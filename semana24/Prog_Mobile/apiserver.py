6f3e010146bd540b0e0d17204a511b088f509ccf
joaochavesjr <joaochaves@gmail.com>
2025-09-22 21:47:09 -0300

    Update Prog Mobile

---
+++ semana24/Prog_Mobile/apiserver.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, send_from_directory
+#from flask_cors import CORS
+
+# Cria a aplicação Flask
+app = Flask(__name__, static_folder='static')
+#CORS(app)
+
+# Rota principal que retorna um JSON
+@app.route("/api/relatorio", methods=["GET"])
+def get_valores():
+    dados = {
+        "Alunos": ["Ana Carolina", "Bruno Marques", "Carlos Eduard", "Diana Lima", 
+                   "José Silva", "Marcos Souza", "Ed Lima"],
+        "Livros": ["Vidas Secas", "Dom Casmurro", "O Cortiço", "Memórias Póstumas de Brás Cubas",
+                   "Vidas Secas", "Dom Casmurro", "O Cortiço"],
+        "Turma": ["3D", "2D", "2E", "3A", "3D", "2D", "2D"]
+    }
+    return jsonify(dados)
+
+# Rota simples de teste
+@app.route("/", methods=["GET"])
+def home():
+    return "<h1>API de Exemplo 🚀</h1><p>Acesse /api/valores para ver os dados em JSON.</p>"
+
+@app.route("/imagem", methods=["GET"])
+def get_imagem():
+    return jsonify({'url': 'http://192.168.10.107:5000/static/images/clean_code.jpg'})
+
+@app.route("/static/images/<path:filename>")
+def serve_image(filename):
+    return send_from_directory(app.static_folder + '/images', filename) 
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
